e31e3114799ead0f912688946ece791f4aef8a03
brrr la arepa <dmenco1234@gmail.com>
2026-05-02 16:14:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
 
-print("salchipapabarco")
-print("Hola desde C")
-print("holaa")
-
-print("Hola desde B")
-print("hola ssss")
-
-print("hoallalaal")
-print("hola rachel")
-
-print("hello ")
-
-print("hoallalaal")
-
-print("dameplata")
-
-
