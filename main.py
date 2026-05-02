97e6a96cf62414b5813084b9d0bc19f855c97c36
AngxxlVs <advs63091@gmail.com>
2026-05-02 16:08:45 -0500

    cambio 1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hola desde B")
-print("hola ssss")
-print("hello ")
+print("hola gente bella")
+a=int(input("ingrese un numero: "))
+b=int(input("ingrese otro numero: "))
+c= a+b
+print(f"la suma de los 2 numeros es {c}")
